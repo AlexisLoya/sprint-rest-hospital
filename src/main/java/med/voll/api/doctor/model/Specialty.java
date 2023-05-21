0769d532d4819e5247d30d8d534725650ec08f23
AlexisLoya <alexloy117@gmail.com>
2023-05-21 16:19:57 -0600

    add specialty class

---
+++ src/main/java/med/voll/api/doctor/model/Specialty.java
@@ -0,0 +1,28 @@
+package med.voll.api.doctor.model;
+
+public enum Specialty {
+    CARDIOLOGIST("Cardiologista"),
+    DERMATOLOGIST("Dermatologista"),
+    ENDOCRINOLOGIST("Endocrinologista"),
+    GASTROENTEROLOGIST("Gastroenterologista"),
+    GYNECOLOGIST("Ginecologista"),
+    HEMATOLOGIST("Hematologista"),
+    NEPHROLOGIST("Nefrologista"),
+    NEUROLOGIST("Neurologista"),
+    OPHTHALMOLOGIST("Oftalmologista"),
+    OTOLARYNGOLOGIST("Otorrinolaringologista"),
+    PEDIATRICIAN("Pediatra"),
+    PULMONOLOGIST("Pneumologista"),
+    UROLOGIST("Urologista");
+
+    private final String description;
+
+    Specialty(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
