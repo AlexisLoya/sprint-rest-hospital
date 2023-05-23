b959dec453108771a9dbc2812a0bed99cdc05a3b
AlexisLoya <alexloy117@gmail.com>
2023-05-22 18:44:20 -0600

    add dependencies and file order

--- src/main/java/med/voll/api/configurations/CorsConfiguration.java
+++ src/main/java/med/voll/api/configurations/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package configurations;
+package med.voll.api.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
