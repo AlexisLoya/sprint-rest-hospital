76f5d7a43829b292c918ed11c56d7cf33be00b48
AlexisLoya <alexloy117@gmail.com>
2023-05-26 20:37:10 -0600

    add error handler validator in Dtos

---
+++ src/main/java/med/voll/api/infra/ErrorHandler.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler({EntityNotFoundException.class, NoSuchElementException.class})
+    public ResponseEntity errorHandler404() {
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity errorHandler400(MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors().stream().map(ErrorDataValidation::new).toList();
+        return ResponseEntity.badRequest()
+                .body(errors);
+    }
+
+    private record ErrorDataValidation(String field, String message) {
+        public ErrorDataValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+
+}
